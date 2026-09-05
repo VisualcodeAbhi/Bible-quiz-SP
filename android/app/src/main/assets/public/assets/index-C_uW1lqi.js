c4c35a874d1367feb8905d638129533f2590daca
Abhi Durgam <abhilash.durgam0@gmil.com>
2026-09-05 21:58:21 +0530

    Bump versionCode to 5 and versionName to 1.0.4 for Play Store release

--- android/app/src/main/assets/public/assets/index-C_uW1lqi.js
+++
@@ -1,2 +0,0 @@
-const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/web-oJJVm55L.js","assets/index-DqUds1MH.js","assets/index-BK5yfUof.css"])))=>i.map(i=>d[i]);
-import{f as e,_ as o}from"./index-DqUds1MH.js";const _=e("Toast",{web:()=>o(()=>import("./web-oJJVm55L.js"),__vite__mapDeps([0,1,2])).then(t=>new t.ToastWeb)});export{_ as Toast};
---
+++ android/app/src/main/assets/public/assets/index-jtdrA1gt.js
@@ -0,0 +1,2 @@
+const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/web-Bt-ZKa49.js","assets/index-q0HHQ2T-.js","assets/index-BK5yfUof.css"])))=>i.map(i=>d[i]);
+import{f as e,_ as o}from"./index-q0HHQ2T-.js";const _=e("Toast",{web:()=>o(()=>import("./web-Bt-ZKa49.js"),__vite__mapDeps([0,1,2])).then(t=>new t.ToastWeb)});export{_ as Toast};
